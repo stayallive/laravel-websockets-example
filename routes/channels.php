5dfe07856115a885f8c24232597b3ce371a9bafa
Alex Bouma <me@alexbouma.me>
2019-01-15 12:12:11 +0100

    Enable broadcasting auth routes

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,6 @@
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('common_room', function ($user = null) {
+    return ['id' => str_random(16)];
 });
