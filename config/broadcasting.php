22c065b6b4c6b4d0c66b6407f399bb7839cb9527
Alex Bouma <me@alexbouma.me>
2021-05-15 21:46:30 +0200

    Update port for Cloudflare

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id'  => env('PUSHER_APP_ID'),
             'options' => [
                 'host'   => '127.0.0.1',
-                'port'   => 6001,
+                'port'   => 6003,
                 'scheme' => 'http',
                 'useTLS' => false, // this is no error, we are talking without SSL to the WebSocket server from Laravel but your end-users will connect with SSL
             ],
